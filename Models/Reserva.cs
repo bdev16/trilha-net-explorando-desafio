b3106e6283586855026f5d74f7d1a3b7d8e20ec3
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-09-30 15:46:28 -0300

    feat(models): implementa o metodo CadastrarHospedes da classe Reserva.cs.
    Esse commit é necessário para implementar o método CadastrarHospedes.
    
    Esse commit implementa a estrutura condional IF/ELSE, utilizando o if
    para verificar se a quantidade de hospedes bate com a quantidade
    permitida pela determinada suite e o else retornando uma exceção que
    indica que a quantidade de hospedes informada acabou ultrapassando a
    permitida pela determinada suite.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count() <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception($"A quantidade de hospedes não pode ultrapassar a quantidade maxima de {Suite.Capacidade} hospedes permitida pela suite");
             }
         }
 
